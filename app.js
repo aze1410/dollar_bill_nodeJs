5990ea0e06a9eeb580164f90e80f1979378e4b8e
aze1410 <riyazeem8x@gmail.com>
2023-11-12 14:14:57 +0530

    modified:   app.js 	modified:   package-lock.json 	modified:   package.json

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Bill=require('./model/bill')
 
 const app=express();
 
-const dbUri="mongodb://localhost:27017/bill";
+const dbUri="mongodb://0.0.0.0:27017/bill";
 
 const billrouters=require('./routes/billRouters');
 const userrouters=require('./routes/userRouters');
